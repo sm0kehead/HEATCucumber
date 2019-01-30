de529a0e4f939d992f9dc184e0c4a258ae19a591
smokehead <artur.wojtowicz1988@gmail.com>
2019-01-30 15:02:08 +0100

    scenario outline

--- src/test/java/CucumberFramework/steps/ContactUsSteps.java
+++ src/test/java/CucumberFramework/steps/ContactUsSteps.java
@@ -7,10 +7,15 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 public class ContactUsSteps {
@@ -32,7 +37,7 @@ public class ContactUsSteps {
     public void i_access_webdriveruniversity() throws Throwable {
         System.out.println("I access webdriveruniversity");
         driver.get("http://webdriveruniversity.com/");
-        Thread.sleep(5000);
+        Thread.sleep(2000);
     }
 
     @When("^I click on the contact us button$")
@@ -41,21 +46,33 @@ public class ContactUsSteps {
         System.out.println("I click on the contact us button");
     }
 
-    @When("^I enter a firstname$")
-    public void i_enter_a_firstname() throws Throwable {
-        //@SupperessWarnings("unused")
-                String winHandleBefore = driver.getWindowHandle();
-        for (String winhandle : driver.getWindowHandles()){
-            driver.switchTo().window(winhandle);
-        }
-        driver.switchTo().frame("contact_form");
-        driver.findElement(By.name("first_name")).sendKeys("Art");
+    @When("^I enter a \"([^\"]*)\" firstname$")
+    public void i_enter_a_firstname(String firstname) throws Throwable {
+        Set<String> st= driver.getWindowHandles();
+        Iterator<String> it = st.iterator();
+        String parent =  it.next();
+        String child =it.next();
+//swtich to parent
+        driver.switchTo().window(parent);
+        System.out.println("Returned to parent");
+// switch to child
+        driver.switchTo().window(child);
+
+       /*String winHandleBefore = driver.getWindowHandle();
+       for (String winHandle : driver.getWindowHandle()) {
+           driver.switchTo().window(winHandle);
+       }*/
+
+       driver.findElement(By.cssSelector("input[name='first_name']")).sendKeys(firstname);
+        //driver.findElement(By.xpath("//input[@name='first_name']")).sendKeys("Arto");
+        //driver.findElement(By.xpath("//input[contains(@name, 'first_name')]")).sendKeys("Arto");
+
         System.out.println("I enter a firstname");
     }
 
-    @When("^I enter a lastname$")
-    public void i_enter_a_lastname() throws Throwable {
-        driver.findElement(By.name("last_name")).sendKeys("Ur");
+    @When("^I enter a \"([^\"]*)\" lastname$")
+    public void i_enter_a_lastname(String lastname) throws Throwable {
+        driver.findElement(By.name("last_name")).sendKeys(lastname);
         System.out.println("I enter a lastname");
     }
 
@@ -66,12 +83,12 @@ public class ContactUsSteps {
     }
 
     @When("^I enter comments$")
-    public void i_enter_comments(DataTable argl) throws Throwable {
-        List<List<String>> data = argl.raw();
-        driver.findElement(By.name("message")).sendKeys(data.get(0).get(0)+ "\n");
-        driver.findElement(By.name("message")).sendKeys(data.get(0).get(1)+ "\n");
-        driver.findElement(By.name("message")).sendKeys(data.get(1).get(0)+ "\n");
-        driver.findElement(By.name("message")).sendKeys(data.get(1).get(1));
+    public void i_enter_comments(DataTable tab1) throws Throwable {
+        List<List<String>> data1 = tab1.raw();
+        driver.findElement(By.name("message")).sendKeys(data1.get(0).get(2)+ "\n");
+        driver.findElement(By.name("message")).sendKeys(data1.get(1).get(2)+ "\n");
+        //driver.findElement(By.name("message")).sendKeys(data.get(1).get(0)+ "\n");
+        //driver.findElement(By.name("message")).sendKeys(data.get(1).get(1));
         System.out.println("I enter comments");
     }
 
@@ -121,8 +138,9 @@ public class ContactUsSteps {
 
     @After()
         public void teardown(){
-
-        driver.quit();
+        //driver.manage().deleteAllCookies();
+        //driver.close();
+        //driver.quit();
     }
 
 
--- target/cucumber/report.js
+++ target/cucumber/report.js
@@ -6,22 +6,13 @@ formatter.feature({
   "id": "submit-data-to-webdriveruniversity.com-using-contact-us-form",
   "keyword": "Feature"
 });
-formatter.before({
-  "duration": 4438095099,
-  "status": "passed"
-});
-formatter.before({
-  "duration": 190359,
-  "error_message": "java.lang.NullPointerException\r\n\tat CucumberFramework.steps.LoginSteps.setup(LoginSteps.java:27)\r\n\tat sun.reflect.NativeMethodAccessorImpl.invoke0(Native Method)\r\n\tat sun.reflect.NativeMethodAccessorImpl.invoke(NativeMethodAccessorImpl.java:62)\r\n\tat sun.reflect.DelegatingMethodAccessorImpl.invoke(DelegatingMethodAccessorImpl.java:43)\r\n\tat java.lang.reflect.Method.invoke(Method.java:498)\r\n\tat cucumber.runtime.Utils$1.call(Utils.java:40)\r\n\tat cucumber.runtime.Timeout.timeout(Timeout.java:16)\r\n\tat cucumber.runtime.Utils.invoke(Utils.java:34)\r\n\tat cucumber.runtime.java.JavaHookDefinition.execute(JavaHookDefinition.java:60)\r\n\tat cucumber.runtime.Runtime.runHookIfTagsMatch(Runtime.java:224)\r\n\tat cucumber.runtime.Runtime.runHooks(Runtime.java:212)\r\n\tat cucumber.runtime.Runtime.runBeforeHooks(Runtime.java:202)\r\n\tat cucumber.runtime.model.CucumberScenario.run(CucumberScenario.java:40)\r\n\tat cucumber.runtime.junit.ExecutionUnitRunner.run(ExecutionUnitRunner.java:102)\r\n\tat cucumber.runtime.junit.FeatureRunner.runChild(FeatureRunner.java:63)\r\n\tat cucumber.runtime.junit.FeatureRunner.runChild(FeatureRunner.java:18)\r\n\tat org.junit.runners.ParentRunner$3.run(ParentRunner.java:290)\r\n\tat org.junit.runners.ParentRunner$1.schedule(ParentRunner.java:71)\r\n\tat org.junit.runners.ParentRunner.runChildren(ParentRunner.java:288)\r\n\tat org.junit.runners.ParentRunner.access$000(ParentRunner.java:58)\r\n\tat org.junit.runners.ParentRunner$2.evaluate(ParentRunner.java:268)\r\n\tat org.junit.runners.ParentRunner.run(ParentRunner.java:363)\r\n\tat cucumber.runtime.junit.FeatureRunner.run(FeatureRunner.java:70)\r\n\tat cucumber.api.junit.Cucumber.runChild(Cucumber.java:95)\r\n\tat cucumber.api.junit.Cucumber.runChild(Cucumber.java:38)\r\n\tat org.junit.runners.ParentRunner$3.run(ParentRunner.java:290)\r\n\tat org.junit.runners.ParentRunner$1.schedule(ParentRunner.java:71)\r\n\tat org.junit.runners.ParentRunner.runChildren(ParentRunner.java:288)\r\n\tat org.junit.runners.ParentRunner.access$000(ParentRunner.java:58)\r\n\tat org.junit.runners.ParentRunner$2.evaluate(ParentRunner.java:268)\r\n\tat org.junit.runners.ParentRunner.run(ParentRunner.java:363)\r\n\tat cucumber.api.junit.Cucumber.run(Cucumber.java:100)\r\n\tat org.junit.runner.JUnitCore.run(JUnitCore.java:137)\r\n\tat com.intellij.junit4.JUnit4IdeaTestRunner.startRunnerWithArgs(JUnit4IdeaTestRunner.java:68)\r\n\tat com.intellij.rt.execution.junit.IdeaTestRunner$Repeater.startRunnerWithArgs(IdeaTestRunner.java:47)\r\n\tat com.intellij.rt.execution.junit.JUnitStarter.prepareStreamsAndStart(JUnitStarter.java:242)\r\n\tat com.intellij.rt.execution.junit.JUnitStarter.main(JUnitStarter.java:70)\r\n",
-  "status": "failed"
-});
-formatter.scenario({
+formatter.scenarioOutline({
   "line": 4,
   "name": "Submit information using contact us form",
   "description": "",
   "id": "submit-data-to-webdriveruniversity.com-using-contact-us-form;submit-information-using-contact-us-form",
-  "type": "scenario",
-  "keyword": "Scenario"
+  "type": "scenario_outline",
+  "keyword": "Scenario Outline"
 });
 formatter.step({
   "line": 5,
@@ -35,12 +26,12 @@ formatter.step({
 });
 formatter.step({
   "line": 7,
-  "name": "I enter a firstname",
+  "name": "I enter a \"\u003c1st\u003e\" firstname",
   "keyword": "And "
 });
 formatter.step({
   "line": 8,
-  "name": "I enter a lastname",
+  "name": "I enter a \"\u003c2nd\u003e\" lastname",
   "keyword": "And "
 });
 formatter.step({
@@ -55,14 +46,141 @@ formatter.step({
     {
       "cells": [
         "1",
-        "2"
+        "2",
+        "ala ma kota"
       ],
       "line": 11
     },
     {
       "cells": [
         "3",
-        "4"
+        "4",
+        "kot ma ale"
+      ],
+      "line": 12
+    }
+  ],
+  "keyword": "And "
+});
+formatter.step({
+  "line": 13,
+  "name": "I click on the submit button",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 14,
+  "name": "the information should successfully be submitted via the contact us form",
+  "keyword": "Then "
+});
+formatter.examples({
+  "line": 16,
+  "name": "",
+  "description": "",
+  "id": "submit-data-to-webdriveruniversity.com-using-contact-us-form;submit-information-using-contact-us-form;",
+  "rows": [
+    {
+      "cells": [
+        "1st",
+        "2nd"
+      ],
+      "line": 17,
+      "id": "submit-data-to-webdriveruniversity.com-using-contact-us-form;submit-information-using-contact-us-form;;1"
+    },
+    {
+      "cells": [
+        "2",
+        "a"
+      ],
+      "line": 18,
+      "id": "submit-data-to-webdriveruniversity.com-using-contact-us-form;submit-information-using-contact-us-form;;2"
+    },
+    {
+      "cells": [
+        "3",
+        "b"
+      ],
+      "line": 19,
+      "id": "submit-data-to-webdriveruniversity.com-using-contact-us-form;submit-information-using-contact-us-form;;3"
+    },
+    {
+      "cells": [
+        "4",
+        "c"
+      ],
+      "line": 20,
+      "id": "submit-data-to-webdriveruniversity.com-using-contact-us-form;submit-information-using-contact-us-form;;4"
+    },
+    {
+      "cells": [
+        "5",
+        "d"
+      ],
+      "line": 21,
+      "id": "submit-data-to-webdriveruniversity.com-using-contact-us-form;submit-information-using-contact-us-form;;5"
+    }
+  ],
+  "keyword": "Examples"
+});
+formatter.before({
+  "duration": 4574999180,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 18,
+  "name": "Submit information using contact us form",
+  "description": "",
+  "id": "submit-data-to-webdriveruniversity.com-using-contact-us-form;submit-information-using-contact-us-form;;2",
+  "type": "scenario",
+  "keyword": "Scenario Outline"
+});
+formatter.step({
+  "line": 5,
+  "name": "I access webdriveruniversity",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 6,
+  "name": "I click on the contact us button",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 7,
+  "name": "I enter a \"2\" firstname",
+  "matchedColumns": [
+    0
+  ],
+  "keyword": "And "
+});
+formatter.step({
+  "line": 8,
+  "name": "I enter a \"a\" lastname",
+  "matchedColumns": [
+    1
+  ],
+  "keyword": "And "
+});
+formatter.step({
+  "line": 9,
+  "name": "I enter an email address",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 10,
+  "name": "I enter comments",
+  "rows": [
+    {
+      "cells": [
+        "1",
+        "2",
+        "ala ma kota"
+      ],
+      "line": 11
+    },
+    {
+      "cells": [
+        "3",
+        "4",
+        "kot ma ale"
       ],
       "line": 12
     }
@@ -83,52 +201,197 @@ formatter.match({
   "location": "ContactUsSteps.i_access_webdriveruniversity()"
 });
 formatter.result({
-  "status": "skipped"
+  "duration": 3093051904,
+  "status": "passed"
 });
 formatter.match({
   "location": "ContactUsSteps.i_click_on_the_contact_us_button()"
 });
 formatter.result({
-  "status": "skipped"
+  "duration": 190953945,
+  "status": "passed"
 });
 formatter.match({
-  "location": "ContactUsSteps.i_enter_a_firstname()"
+  "arguments": [
+    {
+      "val": "2",
+      "offset": 11
+    }
+  ],
+  "location": "ContactUsSteps.i_enter_a_firstname(String)"
 });
 formatter.result({
-  "status": "skipped"
+  "duration": 1131571117,
+  "status": "passed"
 });
 formatter.match({
-  "location": "ContactUsSteps.i_enter_a_lastname()"
+  "arguments": [
+    {
+      "val": "a",
+      "offset": 11
+    }
+  ],
+  "location": "ContactUsSteps.i_enter_a_lastname(String)"
 });
 formatter.result({
-  "status": "skipped"
+  "duration": 46462499,
+  "status": "passed"
 });
 formatter.match({
   "location": "ContactUsSteps.i_enter_an_email_address()"
 });
 formatter.result({
-  "status": "skipped"
+  "duration": 113363042,
+  "status": "passed"
 });
 formatter.match({
   "location": "ContactUsSteps.i_enter_comments(DataTable)"
 });
 formatter.result({
-  "status": "skipped"
+  "duration": 121635263,
+  "status": "passed"
 });
 formatter.match({
   "location": "ContactUsSteps.i_click_on_the_submit_button()"
 });
 formatter.result({
-  "status": "skipped"
+  "duration": 74393,
+  "status": "passed"
 });
 formatter.match({
   "location": "ContactUsSteps.the_information_should_successfully_be_submitted_via_the_contact_us_form()"
 });
 formatter.result({
-  "status": "skipped"
-});
-formatter.after({
-  "duration": 680458685,
+  "duration": 89345,
   "status": "passed"
 });
+formatter.after({
+  "duration": 34279,
+  "status": "passed"
+});
+formatter.before({
+  "duration": 3785269803,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 19,
+  "name": "Submit information using contact us form",
+  "description": "",
+  "id": "submit-data-to-webdriveruniversity.com-using-contact-us-form;submit-information-using-contact-us-form;;3",
+  "type": "scenario",
+  "keyword": "Scenario Outline"
+});
+formatter.step({
+  "line": 5,
+  "name": "I access webdriveruniversity",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 6,
+  "name": "I click on the contact us button",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 7,
+  "name": "I enter a \"3\" firstname",
+  "matchedColumns": [
+    0
+  ],
+  "keyword": "And "
+});
+formatter.step({
+  "line": 8,
+  "name": "I enter a \"b\" lastname",
+  "matchedColumns": [
+    1
+  ],
+  "keyword": "And "
+});
+formatter.step({
+  "line": 9,
+  "name": "I enter an email address",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 10,
+  "name": "I enter comments",
+  "rows": [
+    {
+      "cells": [
+        "1",
+        "2",
+        "ala ma kota"
+      ],
+      "line": 11
+    },
+    {
+      "cells": [
+        "3",
+        "4",
+        "kot ma ale"
+      ],
+      "line": 12
+    }
+  ],
+  "keyword": "And "
+});
+formatter.step({
+  "line": 13,
+  "name": "I click on the submit button",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 14,
+  "name": "the information should successfully be submitted via the contact us form",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "ContactUsSteps.i_access_webdriveruniversity()"
+});
+formatter.result({
+  "duration": 2723941813,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ContactUsSteps.i_click_on_the_contact_us_button()"
+});
+formatter.result({
+  "duration": 172111342,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "3",
+      "offset": 11
+    }
+  ],
+  "location": "ContactUsSteps.i_enter_a_firstname(String)"
+});
+formatter.result({
+  "duration": 1134764185,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "b",
+      "offset": 11
+    }
+  ],
+  "location": "ContactUsSteps.i_enter_a_lastname(String)"
+});
+formatter.result({
+  "duration": 79088247,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ContactUsSteps.i_enter_an_email_address()"
+});
+formatter.result({
+  "duration": 95598774,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ContactUsSteps.i_enter_comments(DataTable)"
 });
